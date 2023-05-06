809d482d08700c5af5d8c7fe77c9fd186d760dfc
Joe Griffin <jgriffin1410@gmail.com>
2023-05-06 15:52:59 +0100

    first commit

---
+++ mixedMessages.js
@@ -0,0 +1,36 @@
+const marcus = "Marcus Aurelius once said: ";
+const epectitus = "Epictetus once said: ";
+const seneca = "Seneca once said: ";
+
+const marcusArray = ["The happiness of your life depends upon the quality of your thoughts.",
+    "Very little is needed to make a happy life; it is all within yourself, in your way of thinking.",
+    "Everything we hear is an opinion, not a fact. Everything we see is a perspective, not the truth.",
+    "The soul becomes dyed with the color of its thoughts.",
+    "The universe is change; our life is what our thoughts make it.",
+    "It is not death that a man should fear, but he should fear never beginning to live.",
+    "The best revenge is to be unlike him who performed the injury.",
+    "The impediment to action advances action. What stands in the way becomes the way.",
+    "The art of living is more like wrestling than dancing.",
+    "The first rule is to keep an untroubled spirit. The second is to look things in the face and know them for what they are."]
+
+const epectitusArray = [    "It's not what happens to you, but how you react to it that matters.",
+"Wealth consists not in having great possessions, but in having few wants.",
+"No man is free who is not master of himself.",
+"He is a wise man who does not grieve for the things which he has not, but rejoices for those which he has.",
+"If anyone tells you that a certain person speaks ill of you, do not make excuses about what is said of you but answer, 'He was ignorant of my other faults, else he would not have mentioned these alone.'",
+"First say to yourself what you would be; and then do what you have to do.",
+"It is impossible for a man to learn what he thinks he already knows.",
+"He who laughs at himself never runs out of things to laugh at.",
+"Freedom is the only worthy goal in life. It is won by disregarding things that lie beyond our control.",
+"Don't seek for everything to happen as you wish it would, but rather wish that everything happens as it actually will—then your life will flow well."]
+
+const senecaArray = [    "It is not that we have a short time to live, but that we waste a lot of it.",
+"Luck is what happens when preparation meets opportunity.",
+"It is the power of the mind to be unconquerable.",
+"To be everywhere is to be nowhere.",
+"It is a rough road that leads to the heights of greatness.",
+"No man was ever wise by chance.",
+"As is a tale, so is life: not how long it is, but how good it is, is what matters.",
+"The whole future lies in uncertainty: live immediately.",
+"It is not that things are difficult that we do not dare, it is because we do not dare that they are difficult.",
+"We suffer more often in imagination than in reality."]
